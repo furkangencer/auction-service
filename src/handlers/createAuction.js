d668c110bb44c96ad5323d6f47c68386ad88c69e
Furkan Gençer <furkan.gencer@getir.com>
2020-10-05 00:06:23 +0300

    Use middy middlewares for lambda

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,11 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createHttpError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 // eslint-disable-next-line no-unused-vars
 const createAuction = async (event, context) => {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
   const auction = {
     id: uuidv4(),
     title,
@@ -13,10 +18,15 @@ const createAuction = async (event, context) => {
     createdAt: (new Date()).toISOString(),
   };
 
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  }).promise();
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch (err) {
+    console.error(err);
+    throw new createHttpError.InternalServerError(err);
+  }
 
   return {
     statusCode: 201,
@@ -24,4 +34,7 @@ const createAuction = async (event, context) => {
   };
 };
 // eslint-disable-next-line import/prefer-default-export
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
