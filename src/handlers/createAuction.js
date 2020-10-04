6454c26b40d0432294f403c943c8d8e908330f51
Furkan Gençer <furkan.gencer@getir.com>
2020-10-04 20:19:27 +0300

    Add dynamodb to store auction object

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,11 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 // eslint-disable-next-line no-unused-vars
 const createAuction = async (event, context) => {
   const { title } = JSON.parse(event.body);
   const auction = {
+    id: uuidv4(),
     title,
     status: 'OPEN',
     createdAt: (new Date()).toISOString(),
   };
+
+  await dynamodb.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise();
+
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
