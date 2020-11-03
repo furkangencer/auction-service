9081aa3c646cd8bddadf43983fdb38fdddd09541
Furkan Gençer <furkan.gencer@getir.com>
2020-11-03 21:56:56 +0300

    Upload auction picture to S3

---
+++ src/handlers/uploadAuctionPicture.js
@@ -0,0 +1,43 @@
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import validator from '@middy/validator';
+import createHttpError from 'http-errors';
+import { getAuctionById } from './getAuction';
+import uploadPictureToS3 from '../lib/uploadPictureToS3';
+import setAuctionPictureUrl from '../lib/setAuctionPictureUrl';
+import uploadAuctionPictureSchema from '../lib/schemas/uploadAuctionPictureSchema';
+
+export const uploadAuctionPicture = async (event) => {
+  const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
+  const auction = await getAuctionById(id);
+
+  // Validate auction ownership
+  if (auction.seller !== email) {
+    throw new createHttpError.Forbidden('You are not the seller of this auction!');
+  }
+
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
+
+  let updatedAuction;
+  try {
+    const pictureUrl = await uploadPictureToS3(`${auction.id}.jpg`, buffer);
+    console.log(pictureUrl);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+  } catch (err) {
+    console.error(err);
+    throw new createHttpError.InternalServerError(err);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+};
+
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler())
+  .use(validator({
+    inputSchema: uploadAuctionPictureSchema,
+  }));
